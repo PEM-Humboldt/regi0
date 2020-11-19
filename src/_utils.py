9cea0598cf0061602023d31a8715aa8a12fcd695
Marcelo <mvilla@humboldt.org.co>
2020-11-19 17:36:25 -0500

    refactored _get_most_recent_year to add more functionalities

--- src/_utils.py
+++ src/_utils.py
@@ -28,12 +28,10 @@ def _create_id_grid(
     """
     height = np.ceil((ymax - ymin) / resolution).astype(np.int)
     width = np.ceil((xmax - xmin) / resolution).astype(np.int)
-
+    transform = rasterio.transform.from_origin(xmin, ymax, resolution, resolution)
     arr = np.arange(height * width, dtype=np.uint32).reshape(height, width)
 
     memfile = rasterio.MemoryFile()
-    transform = rasterio.transform.from_origin(xmin, ymax, resolution, resolution)
-
     grid = memfile.open(
         driver="MEM",
         height=height,
@@ -48,8 +46,11 @@ def _create_id_grid(
     return grid
 
 
-def _get_most_recent_year(
-    dates: pd.Series, unique_years: list, round_unmatched=False
+def _get_nearest_year(
+    dates: pd.Series,
+    unique_years: list,
+    direction: str = "nearest",
+    round_unmatched=False
 ) -> pd.Series:
     """
 
@@ -57,6 +58,7 @@ def _get_most_recent_year(
     ----------
     dates
     unique_years
+    direction
     round_unmatched
 
     Returns
@@ -67,16 +69,20 @@ def _get_most_recent_year(
     This function is based on the answer given on:
     https://stackoverflow.com/a/64881346/7144368
     """
+
     years = pd.to_datetime(dates).dt.year
     years = years.sort_values()
 
     dummy_df = pd.DataFrame({years.name: unique_years, "__year": unique_years})
-    result = pd.merge_asof(years, dummy_df, on=years.name)["__year"]
+    result = pd.merge_asof(years, dummy_df, on=years.name, direction=direction)
 
     if round_unmatched:
-        result[result.isna()] = min(unique_years)
+        if direction == "backward":
+            result[result.isna()] = min(unique_years)
+        elif direction == "forward":
+            result[result.isna()] = max(unique_years)
 
     result.index = years.index
     result = result.sort_index()
 
-    return result.astype(int)
+    return result["__year"].astype(int)
