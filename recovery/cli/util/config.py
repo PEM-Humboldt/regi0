671481f41b6c0e84748131b29bb1c26b9e9103a0
Marcelo <mvilla@humboldt.org.co>
2020-12-16 18:07:56 -0500

    Read template config file (settings.ini) when user configuration is not available.

--- recovery/cli/util/config.py
+++ recovery/cli/util/config.py
@@ -4,6 +4,7 @@ Create config object so CLI commands can access it.
 
 import configparser
 import os
+import pathlib
 
 import appdirs
 
@@ -12,3 +13,7 @@ CONFIG = configparser.ConfigParser()
 CONFIG_PATH = os.path.join(appdirs.user_config_dir("recovery"), "settings.ini")
 if os.path.exists(CONFIG_PATH):
     CONFIG.read(CONFIG_PATH)
+else:
+    root = pathlib.Path(__file__).parent.parent.parent.parent
+    template_config_file = root.joinpath("config").joinpath("settings.ini")
+    CONFIG.read(template_config_file)
