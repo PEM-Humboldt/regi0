c0d7175ea53dabd3326a513548ca835bd4cfd4c8
Marcelo <mvilla@humboldt.org.co>
2021-01-05 09:44:41 -0500

    Add docstrings for the check_species function.

--- recovery/taxonomic.py
+++ recovery/taxonomic.py
@@ -19,22 +19,27 @@ def check_species(
     **kwargs
 ) -> pd.DataFrame:
     """
+    Checks that the scientific names of records are valid using the
+    Global Names Resolver API.
 
     Parameters
     ----------
-    df
-    species_col
-    flag_name
-    add_suggested:
-    suggested_name:
-    add_source
-    source_name
-    drop
+    df:             DataFrame with records.
+    species_col:    Column name with the species name for each record.
+    flag_name:      Column name for the flag.
+    add_suggested:  Whether to add suggested names for records where the
+                    flag is True.
+    suggested_name: Column name for the suggestion if values do not match.
+    add_source:     Whether to add the verification source for all the
+                    records.
+    source_name:    Name for the source column.
+    drop:           Whether to drop records where the flag is True.
     kwargs:         Keyword arguments passed to the gnr_resolve function.
 
     Returns
     -------
-
+    Copy of original DataFrame or a subset (if drop is True) with extra
+    columns.
     """
     # Make sure to modify a copy of the original DataFrame instead of
     # modifying it in place.
