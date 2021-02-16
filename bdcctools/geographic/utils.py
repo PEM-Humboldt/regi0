5855bd7f52f0e942c3725e58daa17a182b240ee8
Marcelo <mvilla@humboldt.org.co>
2021-02-16 09:48:15 -0500

    Rename files and delete existing tests.

--- bdcctools/geographic/utils.py
+++ bdcctools/geographic/utils.py
@@ -6,7 +6,6 @@ import re
 import numpy as np
 import pandas as pd
 import rasterio
-import requests
 from scipy import stats
 
 
@@ -192,83 +191,6 @@ def get_nearest_year(
     result = result.sort_index()
 
     return result
-
-
-def gnr_resolve(
-    names: list,
-    data_source_ids: list = None,
-    resolve_once: bool = False,
-    best_match_only: bool = False,
-    with_context: bool = False,
-    with_vernaculars: bool = False,
-    with_canonical_ranks: bool = False
-) -> list:
-    """
-    Receives a list of names and resolves each against the entire resolver
-    database or against specific data sources using the Global Names
-    Resolver (GNR) API. Underlying resolving and scoring algorithms are
-    described at: http://resolver.globalnames.org/about
-
-    Parameters
-    ----------
-    names:                  List of species names to resolve.
-    data_source_ids:        List of specific data sources IDs to resolve
-                            against. A list of all the available data
-                            sources and their IDs can be found at:
-                            http://resolver.globalnames.org/data_sources.
-    resolve_once:           Find the first available match instead of
-                            matches across all data sources with all
-                            possible renderings of a name.
-    best_match_only:        Returns just one result with the highest
-                            score.
-    with_context:           Reduce the likelihood of matches to taxonomic
-                            homonyms. When True, a common taxonomic
-                            context is calculated for all supplied names
-                            from matches in data sources that have
-                            classification tree paths. Names out of
-                            determined context are penalized during
-                            score calculation.
-    with_vernaculars:       Return 'vernacular' field to present common
-                            names provided by a data source for a
-                            particular match.
-    with_canonical_ranks:   Returns 'canonical_form' with infraspecific
-                            ranks, if they are present.
-
-    Returns
-    -------
-    List with the results for each name in names.
-
-    Notes
-    -----
-    More information on the GNR API can be found at:
-    http://resolver.globalnames.org/api
-    """
-    if data_source_ids is None:
-        data_source_ids = []
-
-    api_url = "http://resolver.globalnames.org/name_resolvers.json"
-
-    # Apparently, GNR API does not accept Booleans so they need to be
-    # converted to lowercase strings first.
-    params = {
-        "data": "\n".join(names),
-        "data_source_ids": "|".join(data_source_ids),
-        "resolve_once": str(resolve_once).lower(),
-        "best_match_only": str(best_match_only).lower(),
-        "with_context": str(with_context).lower(),
-        "with_vernaculars": str(with_vernaculars).lower(),
-        "with_canonical_ranks": str(with_canonical_ranks).lower()
-    }
-
-    try:
-        response = requests.post(api_url, json=params)
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        raise Exception(f"Error calling Global Name Resolver API. {err}")
-
-    data = response.json()["data"]
-
-    return pd.json_normalize(data, record_path="results", meta="supplied_name_string")
 
 
 def is_outlier(
