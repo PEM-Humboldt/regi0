d27d23a33a72be766c43e9400cb919f1174025d1
Marcelo <mvilla@humboldt.org.co>
2021-03-09 08:43:00 -0500

    Add general verification function.

---
+++ bdcctools/utils.py
@@ -0,0 +1,46 @@
+"""
+
+"""
+from typing import Union
+
+import geopandas as gpd
+import pandas as pd
+
+
+def verify(
+    df: Union[gpd.GeoDataFrame, pd.DataFrame],
+    observed_col: str,
+    expected: pd.Series,
+    flag_name: str,
+    add_suggested: bool = False,
+    suggested_name: str = None,
+    drop: bool = False
+) -> Union[gpd.GeoDataFrame, pd.DataFrame]:
+    """
+
+    Parameters
+    ----------
+    df
+    observed_col
+    expected
+    flag_name
+    add_suggested
+    suggested_name
+    drop
+
+    Returns
+    -------
+
+    """
+    # Make sure to modify a copy of the original DataFrame instead of
+    # modifying it in place.
+    df = df.copy()
+
+    df[flag_name] = df[observed_col] == expected
+    if add_suggested:
+        df[suggested_name] = expected
+
+    if drop:
+        df = df[~df[flag_name]]
+
+    return df
