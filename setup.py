8168249a3853fb5545f999f6c25736920acb5dfb
Marcelo Villa-Piñeros <mvilla@humboldt.org.co>
2021-06-21 20:27:37 -0500

    Create setuptools necessary files.

---
+++ setup.py
@@ -0,0 +1,8 @@
+"""
+Required file to install package in development mode.
+
+See: https://setuptools.readthedocs.io/en/latest/userguide/quickstart.html#development-mode
+"""
+import setuptools
+
+setuptools.setup()
