3bac0a00f429882af62ce9ef3fc70c579b69b545
Marcelo Villa-Piñeros <mvilla@humboldt.org.co>
2021-07-23 20:06:46 -0500

    Include qualifiers in get_canonical_name function.

---
+++ bdcctools/taxonomic/parsing.py
@@ -0,0 +1,32 @@
+"""
+Scientific name parsing functions.
+"""
+import pandas as pd
+
+from bdcctools.taxonomic.constants.qualifiers import QUALIFIERS
+from bdcctools.utils import clean_text
+
+
+def get_canonical_name(names: pd.Series) -> pd.Series:
+    """
+
+    Parameters
+    ----------
+    names
+
+    Returns
+    -------
+
+    """
+    names = clean_text(names)
+    names = names.str.capitalize()
+
+    qualifiers = pd.Series(sum(QUALIFIERS.values(), []))
+    qualifiers = qualifiers.str.replace(".", "", regex=False)
+    qualifiers = pd.unique(qualifiers.str.split(" ", expand=True).stack())
+    names = names.str.split(" ", expand=True).replace(qualifiers, "")
+    names = names.fillna("")
+    names = names.apply(lambda x: " ".join(x), axis=1)
+    names = clean_text(names)
+
+    return names.str.split(" ", n=2).str.join(" ")
