3bd6f4e2fb0e82ed94080f5581f186b620cbf5a8
Marcelo Villa-Piñeros <mvilla@humboldt.org.co>
2021-07-27 11:41:50 -0500

    Modify imports to include functions from verification.py.

--- bdcctools/__init__.py
+++ bdcctools/__init__.py
@@ -1,8 +1,4 @@
 import bdcctools.geographic
 import bdcctools.taxonomic
-from bdcctools.io import (
-    read_geographic_table,
-    read_table,
-    write_table
-)
-from bdcctools.utils import verify
+from bdcctools.io import read_geographic_table, read_table, write_table
+from bdcctools.verification import match, verify
