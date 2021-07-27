91f90cc2297145df1bccd6713a771d623f66ded8
Marcelo Villa-Piñeros <mvilla@humboldt.org.co>
2021-07-27 11:41:21 -0500

    Refactor verify function.

--- bdcctools/verification.py
+++ bdcctools/verification.py
@@ -7,17 +7,50 @@ from rapidfuzz import fuzz
 from bdcctools.utils import standardize_text
 
 
+def match(
+    left: pd.Series,
+    right: pd.Series,
+    preprocess: bool = False,
+    fuzzy: bool = False,
+    threshold: float = 0.8,
+) -> pd.Series:
+    """
+
+    Parameters
+    ----------
+    left
+    right
+    preprocess
+    fuzzy
+    threshold
+
+    Returns
+    -------
+
+    """
+    if preprocess:
+        left = standardize_text(left)
+        right = standardize_text(right)
+
+    if fuzzy:
+        values = pd.DataFrame({"left": left, "right": right})
+        score = values.apply(lambda row: fuzz.ratio(row["left"], row["right"]), axis=1)
+        result = (score / 100) >= threshold
+    else:
+        result = left == right
+
+    return result
+
+
 def verify(
     df: pd.DataFrame,
     observed_col: str,
     expected: pd.Series,
     flag_name: str,
-    preprocess: bool = False,
-    fuzzy: bool = False,
-    threshold: float = 0.8,
     add_suggested: bool = False,
     suggested_name: str = None,
-    drop: bool = False
+    drop: bool = False,
+    **kwargs
 ) -> pd.DataFrame:
     """
     Verifies that the values in a specific column from `df` match some
@@ -31,9 +64,6 @@ def verify(
                     length.
     flag_name:      Name of the resulting column indicating whether the
                     observed values match the expected values.
-    preprocess:
-    fuzzy:
-    threshold:
     add_suggested:  Whether to add a column to the result with suggested
                     values for those rows where the observed values do not
                     match the expected values.
@@ -41,6 +71,7 @@ def verify(
                     effect when add_suggested=True is passed.
     drop:           Whether to drop the rows where the observed values
                     do not match the expected values.
+    kwargs:         Keyword arguments accepted by the match function.
 
     Returns
     -------
@@ -48,24 +79,11 @@ def verify(
     """
     df = df.copy()
 
-    observed = df[observed_col].copy()
-    if preprocess:
-        observed = standardize_text(observed)
-        expected = standardize_text(expected)
-
-    if fuzzy:
-        values = pd.DataFrame({"left": observed, "right": expected})
-        score = values.apply(lambda row: fuzz.ratio(row["left"], row["right"]), axis=1)
-        score /= 100
-        match = score >= threshold
-    else:
-        match = observed == expected
-
-    df[flag_name] = match
+    df[flag_name] = match(df[observed_col], expected, **kwargs)
 
     if add_suggested:
-        df.loc[~match, suggested_name] = expected.loc[~match]
+        df.loc[~df[flag_name], suggested_name] = expected.loc[~df[flag_name]]
     if drop:
-        df = df[~match]
+        df = df[~df[flag_name]]
 
     return df
