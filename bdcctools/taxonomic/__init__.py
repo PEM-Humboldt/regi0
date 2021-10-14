552900b152949ecd3eaed4a7737559f7ea04f02f
Marcelo Villa-Piñeros <mvilla@humboldt.org.co>
2021-10-14 09:13:20 -0500

    Remove bdcctools folder.

--- bdcctools/taxonomic/__init__.py
+++
@@ -1,8 +0,0 @@
-from bdcctools.taxonomic.local import (
-    get_checklist_fields,
-    get_checklist_fields_multiple,
-    is_in_checklist,
-    is_in_checklist_multiple
-)
-from bdcctools.taxonomic.parsing import get_canonical_name
-from bdcctools.taxonomic.web import gnr, iucn, speciesplus
