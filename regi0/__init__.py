57cac0215933e68f7cfc4c728e2ce8f189e580b9
Marcelo Villa-Piñeros <mvilla@humboldt.org.co>
2022-01-18 07:09:32 -0500

    Update imports.

--- regi0/__init__.py
+++ regi0/__init__.py
@@ -1,4 +1,5 @@
 import regi0.geographic
 import regi0.taxonomic
-from regi0.io import read_geographic_table, read_table, write_table
+from regi0.readers import read_geographic_table, read_table
 from regi0.verification import match, verify
+from regi0.writers import write_table
