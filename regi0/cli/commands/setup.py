37e43982424574d6185e2fdf68c601169399bbbc
Marcelo Villa-Piñeros <mvilla@humboldt.org.co>
2022-01-28 07:23:36 -0500

    Fix root path and add option to avoid overwriting the configuration file.

--- regi0/cli/commands/setup.py
+++ regi0/cli/commands/setup.py
@@ -11,15 +11,31 @@ from regi0.cli.utils.logger import logger
 
 
 @click.command()
-def setup():
+@click.option(
+    "-o",
+    "--overwrite",
+    is_flag=True,
+    default=False,
+    show_default=True,
+    help="Overwrites any existing configuration file.",
+)
+def setup(overwrite):
     """
     Sets up the configuration file required to run other CLI commands.
     """
-    root = pathlib.Path(__file__).parents[0]
+    root = pathlib.Path(__file__).parents[1]
     config_file = root.joinpath("config/settings.ini")
 
     config_folder = pathlib.Path(appdirs.user_config_dir("regi0"))
     config_folder.mkdir(parents=True, exist_ok=True)
-    shutil.copy(config_file, config_folder)
 
-    logger.info(f"Copied configuration files to {config_folder}")
+    if config_folder.joinpath("settings.ini").exists():
+        if not overwrite:
+            logger.warning(
+                f"Configuration file already exists in {config_folder}. Run this command "
+                f"with the -o/--overwrite flag to overwrite it."
+            )
+            return
+
+    shutil.copy(config_file, config_folder)
+    logger.info(f"Copied configuration file to {config_folder}")
