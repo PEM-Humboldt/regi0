2fff34e11e10ff7fa810523f9a3659439e00ed9e
Marcelo Villa-Piñeros <mvilla@humboldt.org.co>
2022-02-28 08:18:48 -0500

    Update get_taxon_concepts to get more information about the most recent concept.

--- regi0/taxonomic/web/speciesplus.py
+++ regi0/taxonomic/web/speciesplus.py
@@ -44,16 +44,15 @@ def _request(url: str, token: str, params: dict) -> requests.Response:
     return response
 
 
-def get_taxon_concepts(
+def get_taxon_concept(
     names: Union[list, pd.Series, str],
     token: str,
-    add_supplied_names: bool = False,
-    add_source: bool = False,
     expand: bool = True,
-) -> pd.DataFrame:
+    language: Union[str, list] = "EN",
+) -> pd.Series:
     """
-    Calls the taxon_concepts endpoint of the Species+/CITES checklist API
-    for multiple scientific names.
+    Get the most recent taxon concept for multiple scientific names using
+    the taxon_concepts endpoint of the Species+/CITES checklist API.
 
     Parameters
     ----------
@@ -61,40 +60,61 @@ def get_taxon_concepts(
         Scientific name(s) to get results for.
     token : str
         Species+/CITES checklist API authentication token.
-    add_supplied_names : bool
-        Add supplied scientific names to the resulting DataFrame.
-    add_source : bool
-        Add source column to the resulting DataFrame.
     expand : bool
         Whether to expand result rows to match `names` size. If False,
         the number of rows will correspond to the number of unique names
         in `names`.
+    language : str or list
+        One or multiple ISO 639-1 codes (codes for the representation of
+        names of languages; e.g. EN or ES) used to filter languages
+        returned for common names.
 
     Returns
     -------
     Series
-        Series with the corresponding cites listings.
+        Series with taxon concept information.
 
     """
     if isinstance(names, (list, str)):
         names = pd.Series(names)
 
+    if isinstance(language, str):
+        language = language
+
     endpoint = urljoin(API_URL, "taxon_concepts")
+
     df = pd.DataFrame()
 
     unique_names = names.dropna().unique()
     for name in unique_names:
-        response = _request(endpoint, token, {"name": name})
-        if response.json().get("taxon_concepts"):
-            result = pd.DataFrame(response.json()["taxon_concepts"])
-        else:
-            result = pd.Series([], dtype="object")
+        result = pd.Series([], dtype="object")
+        response = _request(
+            endpoint, token, {"name": name, "language": ",".join(language)}
+        )
+        taxon_concepts = response.json()["taxon_concepts"]
+        if taxon_concepts:
+            taxon_concept = sorted(
+                taxon_concepts, key=lambda x: x["updated_at"], reverse=True
+            )[0]
+            result.loc["id"] = taxon_concept.get("id")
+            result.loc["author_year"] = taxon_concept.get("author_year")
+            result.loc["cites_listing"] = taxon_concept.get("cites_listing")
+            result.loc["common_names"] = "|".join(
+                [item.get("name") for item in taxon_concept.get("common_names")]
+            )
+            result.loc["synonyms"] = "|".join(
+                [
+                    item.get("full_name")
+                    for item in taxon_concept.get("synonyms")
+                    if item.get("rank") == "SPECIES"
+                ]
+            )
+            if taxon_concept.get("higher_taxa"):
+                for rank, value in taxon_concept.get("higher_taxa").items():
+                    result.loc[rank] = value
+
         df = df.append(result, ignore_index=True)
 
-    if add_supplied_names:
-        df["supplied_name"] = unique_names
-    if add_source:
-        df["source"] = "Species+/CITES"
     if expand:
         df = expand_result(df, names)
 
