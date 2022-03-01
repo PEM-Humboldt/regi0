b611a7b8bfe50a7e6a60b12db885fa8e59fbc9c0
Marcelo Villa-Piñeros <mvilla@humboldt.org.co>
2022-03-01 05:39:05 -0500

    Keep only one success test case that covers the add_supplied_names and add_source arguments.

--- tests/taxonomic/web/gnr/test_get_classification.py
+++ tests/taxonomic/web/gnr/test_get_classification.py
@@ -8,49 +8,11 @@ import pytest
 from regi0.taxonomic.web.gnr import get_classification
 
 
-def test_defaults(success):
-    result = get_classification(
-        ["Panthera onca", "Tremarctos ornatus"], data_source_ids=["1"]
-    )
-    expected = pd.DataFrame(
-        {
-            "kingdom": ["Animalia", "Animalia"],
-            "phylum": ["Chordata", "Chordata"],
-            "class": ["Mammalia", "Mammalia"],
-            "order": ["Carnivora", "Carnivora"],
-            "family": ["Felidae", "Ursidae"],
-            "genus": ["Panthera", "Tremarctos"],
-            "species": ["Panthera onca", "Tremarctos ornatus"],
-        }
-    )
-    pd.testing.assert_frame_equal(result, expected)
-
-
-def test_add_supplied_name(success):
+def test_success(success):
     result = get_classification(
         ["Panthera onca", "Tremarctos ornatus"],
         data_source_ids=["1"],
         add_supplied_names=True,
-    )
-    expected = pd.DataFrame(
-        {
-            "kingdom": ["Animalia", "Animalia"],
-            "phylum": ["Chordata", "Chordata"],
-            "class": ["Mammalia", "Mammalia"],
-            "order": ["Carnivora", "Carnivora"],
-            "family": ["Felidae", "Ursidae"],
-            "genus": ["Panthera", "Tremarctos"],
-            "species": ["Panthera onca", "Tremarctos ornatus"],
-            "supplied_name": ["Panthera onca", "Tremarctos ornatus"],
-        }
-    )
-    pd.testing.assert_frame_equal(result, expected)
-
-
-def test_add_source(success):
-    result = get_classification(
-        ["Panthera onca", "Tremarctos ornatus"],
-        data_source_ids=["1"],
         add_source=True,
     )
     expected = pd.DataFrame(
@@ -62,6 +24,7 @@ def test_add_source(success):
             "family": ["Felidae", "Ursidae"],
             "genus": ["Panthera", "Tremarctos"],
             "species": ["Panthera onca", "Tremarctos ornatus"],
+            "supplied_name": ["Panthera onca", "Tremarctos ornatus"],
             "source": ["Catalogue of Life Checklist", "Catalogue of Life Checklist"],
         }
     )
