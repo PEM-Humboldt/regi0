f0844eb14924ecf3953298db17b6c769dea326ef
Marcelo Villa-Piñeros <mvilla@humboldt.org.co>
2022-03-01 02:43:10 -0500

    Move json responses inside class methods.

--- tests/geographic/web/arcgis/conftest.py
+++ tests/geographic/web/arcgis/conftest.py
@@ -4,55 +4,6 @@ Configuration file for the regi0.geographic.web.arcgis module tests.
 import pytest
 import requests
 
-success_json = {
-    "type": "FeatureCollection",
-    "features": [
-        {
-            "id": "0",
-            "type": "Feature",
-            "properties": {"dptos": "SANTANDER", "fid": 4.0},
-            "geometry": {
-                "type": "Polygon",
-                "coordinates": [
-                    [
-                        [-73.21752935323963, 6.562170940611484],
-                        [-74.57644315124725, 6.2039350030896525],
-                        [-73.77704340199992, 8.142174027000065],
-                        [-71.95538682324725, 6.996811748089653],
-                        [-73.21752935323963, 6.562170940611484],
-                    ]
-                ],
-            },
-        },
-        {
-            "id": "1",
-            "type": "Feature",
-            "properties": {"dptos": "BOYACA", "fid": 22.0},
-            "geometry": {
-                "type": "Polygon",
-                "coordinates": [
-                    [
-                        [-73.03057881399992, 4.982293329000066],
-                        [-74.65938395399992, 5.748359847000065],
-                        [-74.57644315124725, 6.2039350030896525],
-                        [-73.21752935323963, 6.562170940611484],
-                        [-71.95538682324725, 6.996811748089653],
-                        [-73.03057881399992, 4.982293329000066],
-                    ]
-                ],
-            },
-        },
-    ],
-}
-
-error_json = {
-    "error": {
-        "code": 400,
-        "message": "Cannot perform query. Invalid query parameters.",
-        "details": ["Unable to perform query. Please check your parameters."],
-    }
-}
-
 
 class SuccessResponse(requests.Response):
     def __init__(self):
@@ -60,7 +11,46 @@ class SuccessResponse(requests.Response):
         self.status_code = 200
 
     def json(self, **kwargs):
-        return success_json
+        return {
+            "type": "FeatureCollection",
+            "features": [
+                {
+                    "id": "0",
+                    "type": "Feature",
+                    "properties": {"dptos": "SANTANDER", "fid": 4.0},
+                    "geometry": {
+                        "type": "Polygon",
+                        "coordinates": [
+                            [
+                                [-73.21752935323963, 6.562170940611484],
+                                [-74.57644315124725, 6.2039350030896525],
+                                [-73.77704340199992, 8.142174027000065],
+                                [-71.95538682324725, 6.996811748089653],
+                                [-73.21752935323963, 6.562170940611484],
+                            ]
+                        ],
+                    },
+                },
+                {
+                    "id": "1",
+                    "type": "Feature",
+                    "properties": {"dptos": "BOYACA", "fid": 22.0},
+                    "geometry": {
+                        "type": "Polygon",
+                        "coordinates": [
+                            [
+                                [-73.03057881399992, 4.982293329000066],
+                                [-74.65938395399992, 5.748359847000065],
+                                [-74.57644315124725, 6.2039350030896525],
+                                [-73.21752935323963, 6.562170940611484],
+                                [-71.95538682324725, 6.996811748089653],
+                                [-73.03057881399992, 4.982293329000066],
+                            ]
+                        ],
+                    },
+                },
+            ],
+        }
 
 
 class ErrorResponse(requests.Response):
@@ -69,7 +59,13 @@ class ErrorResponse(requests.Response):
         self.status_code = 200
 
     def json(self, **kwargs):
-        return error_json
+        return {
+            "error": {
+                "code": 400,
+                "message": "Cannot perform query. Invalid query parameters.",
+                "details": ["Unable to perform query. Please check your parameters."],
+            }
+        }
 
 
 class BadRequest(requests.Response):
--- tests/taxonomic/web/gnr/conftest.py
+++ tests/taxonomic/web/gnr/conftest.py
@@ -4,72 +4,6 @@ Configuration file for the regi0.taxonomic.web.gnr module tests.
 import pytest
 import requests
 
-success_json = {
-    "id": "zves42fc59vn",
-    "url": "http://resolver.globalnames.org/name_resolvers/zves42fc59vn.json",
-    "data_sources": [{"id": 1, "title": "Catalogue of Life Checklist"}],
-    "data": [
-        {
-            "supplied_name_string": "Panthera onca",
-            "is_known_name": True,
-            "results": [
-                {
-                    "data_source_id": 1,
-                    "data_source_title": "Catalogue of Life Checklist",
-                    "gni_uuid": "635db626-3f58-5caa-8148-4d77cd81d98d",
-                    "name_string": "Panthera onca (Linnaeus, 1758)",
-                    "canonical_form": "Panthera onca",
-                    "classification_path": "Biota|Animalia|Chordata|Mammalia|Theria|Eutheria|Carnivora|Feliformia|Felidae|Pantherinae|Panthera|Panthera onca",
-                    "classification_path_ranks": "unranked|kingdom|phylum|class|subclass|infraclass|order|suborder|family|subfamily|genus|species",
-                    "classification_path_ids": "5T6MX|N|CH2|6224G|6226C|LG|VS|4DL|623RM|628LP|6DBT|4CGXQ",
-                    "taxon_id": "4CGXQ",
-                    "edit_distance": 0,
-                    "imported_at": "2021-11-20T18:36:08Z",
-                    "match_type": 2,
-                    "match_value": "Exact match by canonical form",
-                    "prescore": "3|0|0",
-                    "score": 0.988,
-                }
-            ],
-        },
-        {
-            "supplied_name_string": "Tremarctos ornatus",
-            "is_known_name": True,
-            "results": [
-                {
-                    "data_source_id": 1,
-                    "data_source_title": "Catalogue of Life Checklist",
-                    "gni_uuid": "c6b3c50b-f013-5125-aed0-81cc6d58b635",
-                    "name_string": "Tremarctos ornatus (F. G. Cuvier, 1825)",
-                    "canonical_form": "Tremarctos ornatus",
-                    "classification_path": "Biota|Animalia|Chordata|Mammalia|Theria|Eutheria|Carnivora|Caniformia|Ursidae|Tremarctos|Tremarctos ornatus",
-                    "classification_path_ranks": "unranked|kingdom|phylum|class|subclass|infraclass|order|suborder|family|genus|species",
-                    "classification_path_ids": "5T6MX|N|CH2|6224G|6226C|LG|VS|4CT|HQQ|7YPQ|5832V",
-                    "taxon_id": "5832V",
-                    "edit_distance": 0,
-                    "imported_at": "2021-11-20T19:03:46Z",
-                    "match_type": 2,
-                    "match_value": "Exact match by canonical form",
-                    "prescore": "3|0|0",
-                    "score": 0.988,
-                }
-            ],
-        },
-    ],
-    "status": "success",
-    "message": "Success",
-    "parameters": {
-        "with_context": False,
-        "header_only": False,
-        "with_canonical_ranks": False,
-        "with_vernaculars": False,
-        "best_match_only": False,
-        "data_sources": [1],
-        "preferred_data_sources": [],
-        "resolve_once": False,
-    },
-}
-
 
 class SuccessResponse(requests.Response):
     def __init__(self):
@@ -77,7 +11,71 @@ class SuccessResponse(requests.Response):
         self.status_code = 200
 
     def json(self, **kwargs):
-        return success_json
+        return {
+            "id": "zves42fc59vn",
+            "url": "http://resolver.globalnames.org/name_resolvers/zves42fc59vn.json",
+            "data_sources": [{"id": 1, "title": "Catalogue of Life Checklist"}],
+            "data": [
+                {
+                    "supplied_name_string": "Panthera onca",
+                    "is_known_name": True,
+                    "results": [
+                        {
+                            "data_source_id": 1,
+                            "data_source_title": "Catalogue of Life Checklist",
+                            "gni_uuid": "635db626-3f58-5caa-8148-4d77cd81d98d",
+                            "name_string": "Panthera onca (Linnaeus, 1758)",
+                            "canonical_form": "Panthera onca",
+                            "classification_path": "Biota|Animalia|Chordata|Mammalia|Theria|Eutheria|Carnivora|Feliformia|Felidae|Pantherinae|Panthera|Panthera onca",
+                            "classification_path_ranks": "unranked|kingdom|phylum|class|subclass|infraclass|order|suborder|family|subfamily|genus|species",
+                            "classification_path_ids": "5T6MX|N|CH2|6224G|6226C|LG|VS|4DL|623RM|628LP|6DBT|4CGXQ",
+                            "taxon_id": "4CGXQ",
+                            "edit_distance": 0,
+                            "imported_at": "2021-11-20T18:36:08Z",
+                            "match_type": 2,
+                            "match_value": "Exact match by canonical form",
+                            "prescore": "3|0|0",
+                            "score": 0.988,
+                        }
+                    ],
+                },
+                {
+                    "supplied_name_string": "Tremarctos ornatus",
+                    "is_known_name": True,
+                    "results": [
+                        {
+                            "data_source_id": 1,
+                            "data_source_title": "Catalogue of Life Checklist",
+                            "gni_uuid": "c6b3c50b-f013-5125-aed0-81cc6d58b635",
+                            "name_string": "Tremarctos ornatus (F. G. Cuvier, 1825)",
+                            "canonical_form": "Tremarctos ornatus",
+                            "classification_path": "Biota|Animalia|Chordata|Mammalia|Theria|Eutheria|Carnivora|Caniformia|Ursidae|Tremarctos|Tremarctos ornatus",
+                            "classification_path_ranks": "unranked|kingdom|phylum|class|subclass|infraclass|order|suborder|family|genus|species",
+                            "classification_path_ids": "5T6MX|N|CH2|6224G|6226C|LG|VS|4CT|HQQ|7YPQ|5832V",
+                            "taxon_id": "5832V",
+                            "edit_distance": 0,
+                            "imported_at": "2021-11-20T19:03:46Z",
+                            "match_type": 2,
+                            "match_value": "Exact match by canonical form",
+                            "prescore": "3|0|0",
+                            "score": 0.988,
+                        }
+                    ],
+                },
+            ],
+            "status": "success",
+            "message": "Success",
+            "parameters": {
+                "with_context": False,
+                "header_only": False,
+                "with_canonical_ranks": False,
+                "with_vernaculars": False,
+                "best_match_only": False,
+                "data_sources": [1],
+                "preferred_data_sources": [],
+                "resolve_once": False,
+            },
+        }
 
 
 class BadRequest(requests.Response):
